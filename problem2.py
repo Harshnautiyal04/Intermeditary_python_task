76d9eed8b0e063f96a4b2c73ee50c88c28e5a528
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-29 23:40:18 +0530

    Intermediary task problem2

---
+++ problem2.py
@@ -0,0 +1,12 @@
+#write a python program to get temperature in fahrenhiet to convert into centigrade and kelvin
+
+fahrenheit = float(input("Enter temperature in F : "))
+
+celsius = (fahrenheit - 32) * 5/9 
+
+kelvin =  (fahrenheit - 32) * 5/9 + 273.15
+
+print(f"The {fahrenheit}f is converting into {celsius}c .")
+
+print(f"The {fahrenheit}f is converting into {kelvin}k.")
+
