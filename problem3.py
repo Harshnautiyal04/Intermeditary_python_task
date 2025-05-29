bef77707cbd65d584d0df01add2d1d7db8727b0b
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-30 00:20:05 +0530

    problem3.py

---
+++ problem3.py
@@ -0,0 +1,9 @@
+# write a pyhton program to get password fromm the user that contain alpha numeric and nore than 8 and less than 20 characters
+
+
+pwd = input("Enter password : ")
+
+if pwd.isalnum() and 8 <= len(pwd) <=20:
+    print(f"The password is {pwd}.")
+else:
+    print(f"Sorry,The password {pwd} is invalid the criteria is not match!")
