ece6acdd9d847f432739990296dfba96f2259bc9
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-06-05 18:02:38 +0530

    Sort dictionary by key

---
+++ problem8.py
@@ -0,0 +1,15 @@
+# write a python program sort dictionary by key
+
+my_dict = {'Name' : 'Harsh', 'Age' : 19 , 'Course' : 'Python' ,'City' : 'New Delhi'}
+
+print(my_dict)
+
+print(sorted(my_dict.keys()))
+
+print(sorted(str(v) for v in my_dict.values()))
+
+print(sorted(my_dict.items()))
+
+
+
+
